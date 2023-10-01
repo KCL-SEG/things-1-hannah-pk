396b7a2c99b4250618ca86c436d5aea1648a76e1
Hannah Porteous <hannah.porteous@kcl.ac.uk>
2023-10-01 20:50:27 +0100

    Edit models

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
+    name = models.TextField()
     description = models.TextField()
     quantity = models.IntegerField()
